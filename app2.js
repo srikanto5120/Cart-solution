3ca44eaf5ff5cd693a844f23e645bb6e562db978
srikanto5120 <shreekantaray@gmail.com>
2021-08-18 20:18:28 +0600

    first commit

---
+++ app2.js
@@ -0,0 +1,48 @@
+ function updateProductNumber(product,price,isIncreasing){
+    const productInput = document.getElementById(product+'-number');
+     productNumber= productInput.value;
+    if(isIncreasing ==true){
+        productNumber = parseInt(productNumber) + 1;
+       
+    }else if(productNumber>0){
+        productNumber = parseInt(productNumber) - 1;
+        
+    }
+    productInput.value = productNumber;
+     const profuctQuntity = document.getElementById(product+'-total');
+     profuctQuntity.innerText= productNumber * price;
+
+     calculateTotal()
+     
+ }
+ function getInputValue(product){
+    const phoneInput = document.getElementById(product+'-number');
+    const productNumber= phoneInput.value;
+    return productNumber
+ }
+
+
+ function calculateTotal(){
+     
+      const phoneTotal= getInputValue('phone') * 1219;
+      const caseTotal= getInputValue('case') * 59;
+      const subTotal = phoneTotal + caseTotal;
+      const tax = subTotal / 10;
+      const totalPrice = subTotal + tax;
+      document.getElementById('sub-total').innerText = subTotal;
+      document.getElementById('tax-amount').innerText = tax;
+      document.getElementById('total-price').innerText = totalPrice;
+       
+ }
+ document.getElementById('case-plus').addEventListener('click',function(){
+     updateProductNumber('case',59,true)
+ })
+ document.getElementById('case-minus').addEventListener('click',function(){
+     updateProductNumber('case',59,false)
+ })
+ document.getElementById('phone-plus').addEventListener('click', function(){
+     updateProductNumber('phone',1219,true)
+ })
+document.getElementById('phone-minus').addEventListener('click',function(){
+    updateProductNumber('phone',1219,false)
+})
